f85fc877d57025f2f9f25b310dffe30efbfc54ba
suicidepills <atom.mac@gmail.com>
2017-01-01 23:02:04 -0500

    Search both project names and authors

--- server/services/project-service.js
+++ server/services/project-service.js
@@ -1,13 +1,14 @@
+const P = require('bluebird');
 const MongoClient = require('mongodb');
 
 let projectsDb = null;
-let users = null;
-const url = 'mongodb://localhost:27017/test';
+let projects = null;
+const url = 'mongodb://localhost:27017/roc-game-dev-gallery';
 MongoClient
   .connect(url)
   .then(function (db) {
     projectsDb = db;
-    users = db.collection('users');
+    projects = db.collection('projects');
   })
   .catch(function (err) {
     console.log('ERROR connecting to MongoDb: ', err);
@@ -16,20 +17,37 @@ MongoClient
 const searchByName = (options = { search: '' }) => {
   if (!projectsDb) return Promise.reject('No database connection.');
   // TODO: Consider replacing this w/ a text index.  Faster?
-  const results = users.find({
-    name: {
-      $regex: '.*' + options.search + '.*',
-      $options: 'i'
-    }
+  const results = P.all([
+    projects.find({
+      name: {
+        $regex: '.*' + options.search + '.*',
+        $options: 'i'
+      }
+    }),
+    projects.find({
+      author: {
+        $regex: '.*' + options.search + '.*',
+        $options: 'i'
+      }
+    })
+  ])
+  .spread((nameResults, authorResults) => {
+    return P.all([
+      nameResults.toArray(),
+      authorResults.toArray()
+    ])
+    .spread((nameResultsArr, authorResultsArr) => {
+      return nameResultsArr.concat(authorResultsArr);
+    });
   });
-  return results.toArray();
+  return results;
 };
 
 const getById = (options = {}) => {
   if (!projectsDb) return Promise.reject('No database connection.');
   if (typeof options.id !== 'string') return Promise.reject('An "id" propery is missing from the options argument');
 
-  return users.findOne({ id: options.id });
+  return projects.findOne({ id: options.id });
 };
 
 module.exports = {
